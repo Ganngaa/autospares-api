e462e5bf73dfe9bc56a11b2ff24ec65ca4b7e4eb
Ganga Anilkumar <165378558+Ganngaa@users.noreply.github.com>
2026-05-30 21:08:22 +0530

    Enable receive stock update

--- receive_order_item.php
+++ receive_order_item.php
@@ -10,4 +10,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
 header("Content-Type: application/json");
 
-echo json_encode($_POST);
+include("db.php");
+
+$item_id = $_POST['item_id'];
+$product_name = $_POST['product_name'];
+$qty = intval($_POST['qty']);
+
+$result1 = $conn->query("
+UPDATE order_items
+SET received_qty = received_qty + $qty
+WHERE id='$item_id'
+");
+
+$result2 = $conn->query("
+UPDATE products
+SET stock = stock + $qty
+WHERE name='$product_name'
+");
+
+echo json_encode([
+    "success" => true,
+    "order_items_updated" => $result1,
+    "products_updated" => $result2,
+    "mysql_error" => $conn->error
+]);
