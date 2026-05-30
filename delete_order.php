7d6bba7e202573e87c4c92aba0b42bad3f38bf4b
Ganga Anilkumar <165378558+Ganngaa@users.noreply.github.com>
2026-05-30 16:07:14 +0530

    Added order delete

--- delete_order.php
+++ delete_order.php
@@ -1,17 +1,29 @@
 <?php
 
+header("Content-Type: application/json");
+
 include("db.php");
 
 $id = $_POST['id'];
 
+// Delete order items first
 $conn->query(
-  "DELETE FROM order_items WHERE order_id='$id'"
+    "DELETE FROM order_items WHERE order_id='$id'"
 );
 
-$conn->query(
-  "DELETE FROM orders WHERE id='$id'"
-);
+// Delete order
+$sql = "DELETE FROM orders WHERE id='$id'";
 
-echo json_encode([
-  "success"=>true
-]);
+if($conn->query($sql)){
+
+    echo json_encode([
+        "success" => true
+    ]);
+
+}else{
+
+    echo json_encode([
+        "success" => false
+    ]);
+}
+?>
