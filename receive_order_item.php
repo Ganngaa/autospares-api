0ac787d627d2c5278cdf272a1304cb5625c66ef6
Ganga Anilkumar <165378558+Ganngaa@users.noreply.github.com>
2026-05-30 21:03:53 +0530

    Debug receive order

--- receive_order_item.php
+++ receive_order_item.php
@@ -1,7 +1,13 @@
 <?php
 
-header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
-include("db.php");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
+
+header("Content-Type: application/json");
 
 echo json_encode($_POST);
